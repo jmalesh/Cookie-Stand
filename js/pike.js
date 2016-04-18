bbd29de4e9fe99d6f93020d054fa40a89112fcda
Jess Malesh <jamalesh84@gmail.com>
2016-04-18 16:04:05 -0700

    it works!

--- js/pike.js
+++ js/pike.js
@@ -1,6 +1,8 @@
 //global functions and variables
 var openHours = ['6am:', '7am:', '8am:', '9am:', '10am:', '11a:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:', '8pm:' ];
 
+var msg;
+
 var pikePlace = {
   locationName: 'Pike\'s Place',
   minCustomer: 17,
@@ -12,22 +14,26 @@ var pikePlace = {
   aveCookiesPerHour: function () {
     return Math.floor((this.aveCustomersIn()) * this.avePerCust);
   },
+  customerArray: [],
+  cookieArray: [],
 };
 
-//console.log(parseInt(pikePlace.aveCustomersIn));
+var newData = document.getElementById('newData');
+for (var i = 0; i < openHours.length; i++) {
+  var openTimes = document.createElement('li');
+  //openTimes.value = newData;
+  //openTimes.textContent = newData;
+  pikePlace.cookieArray.push(pikePlace.aveCookiesPerHour());
+  openTimes.textContent = (openHours[i] + pikePlace.cookieArray[i] + ' cookies');
+  newData.appendChild(openTimes);
+}
 
-//var elCustomers = document.getElementById('newData');
-//elCustomers.textContent = pikePlace.aveCustomersIn;
 //
-// var totalCustomers = function getRandomIntInclusive(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// };
-//
-// var newData = document.getElementById('newData');
-// 	for (var i = pikePlace.minCustomer; i < pikePlace.maxCustomer; i++) {
-//   var digits = i.toString();
-//   var newOpt = document.createElement('option');
-//   newOpt.value = digits;
-//   newOpt.textContent = digits + ' cookies';
-//   newData.appendChild(newOpt);
-// }
+// var elCustomers = document.getElementById('newData');
+// elCustomers.textContent = pikePlace.aveCustomersIn;
+// //
+// // var newData = document.getElementById('newData');
+// var newOpt = document.createElement('option');
+// //   newOpt.value = digits;
+// //   newOpt.textContent = digits + ' cookies';
+// //   newData.appendChild(newOpt);
